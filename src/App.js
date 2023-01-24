4cb907a6d0d019a0825456580bf55fbeb22a3c1f
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-24 12:17:47 +0500

    Update App.js
    Done With random Quote generator USING API

--- src/App.js
+++ src/App.js
@@ -3,29 +3,6 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
-
-  // state = {
-  //   quote : ' '
-  // };
-
-  // componentDidMount(){
-  //   this.fetchQuote()
-  // }
-
-  // fetchQuote = () =>{
-  //   axios.get("https://api.adviceslip.com/advice")
-  //   .then((response) =>{
-  //    const { quote } = response.data.slip;
-  //         this.setState({ quote });
-  //        console.log(response.data.slip.advice)
-  //        this.setState({ quote });
-  //   })
-  //   .catch((error) =>{
-  //     alert(error)
-  //   } )
-  // }
-
-
   state = {
     advice: "",
   };
@@ -38,11 +15,8 @@ export default class App extends Component {
     axios
       .get("https://api.adviceslip.com/advice")
       .then((response) => {
-        // const { advice } = response.data.slip;
-
-        // this.setState({ advice });
         const { advice } = response.data.slip;
-        // console.log(response.data.slip.advice)
+        console.log(response.data.slip.advice)
 
         this.setState({ advice });
       })
@@ -52,20 +26,11 @@ export default class App extends Component {
   };
 
   render() {
-    // const {quote} = this.state
+    const {advice} = this.state
     return (
-
-      // <div className="app">
-      // <div className="card">
-      // <h1 className="heading">{quote}</h1>
-      // <button className="button" onClick={this.fetchQuote}>
-      //       <span>Generate a Quote</span>
-      //     </button>
-      // </div>
-      // </div>
       <div className="app">
         <div className="card">
-          <h1 className="heading">{this.state.advice}</h1>
+          <h1 className="heading">{advice}</h1>
           <button className="button" onClick={this.fetchAdvice}>
             <span>Generate a Quote</span>
           </button>
