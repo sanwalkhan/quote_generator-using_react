d1dc9f697fba951a95641576d0c30e48bf9e9eb0
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-24 11:54:31 +0500

    Update App.js
    using api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  // state = {
+  //   quote : ' '
+  // };
+
+  // componentDidMount(){
+  //   this.fetchQuote()
+  // }
+
+  // fetchQuote = () =>{
+  //   axios.get("https://api.adviceslip.com/advice")
+  //   .then((response) =>{
+  //    const { quote } = response.data.slip;
+  //         this.setState({ quote });
+  //        console.log(response.data.slip.advice)
+  //        this.setState({ quote });
+  //   })
+  //   .catch((error) =>{
+  //     alert(error)
+  //   } )
+  // }
+
+
   state = {
-    advice : ''
+    advice: "",
   };
 
   componentDidMount() {
@@ -12,32 +35,42 @@ export default class App extends Component {
   }
 
   fetchAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice')
+    axios
+      .get("https://api.adviceslip.com/advice")
       .then((response) => {
         // const { advice } = response.data.slip;
 
         // this.setState({ advice });
-        const {advice} = response.data.slip
-        console.log(response.data.slip.advice)
+        const { advice } = response.data.slip;
+        // console.log(response.data.slip.advice)
 
-        this.setState({advice})
+        this.setState({ advice });
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
   render() {
+    // const {quote} = this.state
+    return (
 
-    
-    return <div className="app">
-    <div className="card">
-    <h1 className="heading">{this.state.advice}</h1>
-    <button className="button" onClick={this.fetchAdvice}>
-    <span>Generate a Quote</span>
-    </button>
-    </div>
-    
-    </div>;
+      // <div className="app">
+      // <div className="card">
+      // <h1 className="heading">{quote}</h1>
+      // <button className="button" onClick={this.fetchQuote}>
+      //       <span>Generate a Quote</span>
+      //     </button>
+      // </div>
+      // </div>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Generate a Quote</span>
+          </button>
+        </div>
+      </div>
+    );
   }
 }
